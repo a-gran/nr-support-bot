f97dd811ef4ee3268082ff86f6733232a5944646
Aristarh Gran <awebdeff@gmail.com>
2023-08-29 21:47:38 +0300

    update

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-	semi: false,
-	singleQuote: true,
-	singleAttributePerLine: true,
-	editor.tabSize: 4,
-}
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api')
-
+const debug = require('./helpers')
 const TOKEN = '6522253030:AAERyOphTX34pacqrJr8tCCMAG0euysYLBU'
 
-console.log('Starting bot...')
-
 const bot = new TelegramBot(TOKEN, {
-  polling: {
-    interval: 300,
-    autoStart: true,
-    params: {
-      timeout: 10,
+    polling: {
+        interval: 300,
+        autoStart: true,
+        params: {
+            timeout: 10,
+        },
     },
-  },
 })
 
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'I am alive!!!') // test
+    bot.sendMessage(msg.chat.id, 'Message!')
 })
